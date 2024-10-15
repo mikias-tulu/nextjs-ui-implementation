4fa34288056d0eaf58441d302f1afe78a5e0efb1
mikias-tulu <miki.tulu@gmail.com>
2024-10-15 13:44:14 +0300

    Add swiper section for slider and hero section swiper

---
+++ components/SwiperSection.tsx
@@ -0,0 +1,120 @@
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SvgDollarIcon from './svgs/svgDollarIcon';
+import SvgArrowDown from './svgs/svgArrowDown';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import 'swiper/css/navigation';
+import { Navigation, EffectCards } from 'swiper/modules';
+import { useState, useEffect } from 'react';
+import SvgTickIcon from './svgs/svgTickIcon';
+
+interface SwiperItem {
+  id: number;
+  proImage: string;
+  topSpan: string;
+  name: string;
+  experience: string;
+  skills: string[];
+}
+
+const SwiperSection: React.FC = () => {
+  const [activeIndex, setActiveIndex] = useState(1);
+  const [swiperData, setSwiperData] = useState<SwiperItem[]>([]);
+
+  useEffect(() => {
+    const fetchSwiperData = async () => {
+      try {
+        const res = await fetch('/api/data');
+        const data = await res.json();
+        setSwiperData(data.swiper); 
+      } catch (error) {
+        console.error('Error fetching swiper data:', error);
+      }
+    };
+
+    fetchSwiperData();
+  }, []);
+
+  return (
+    <>
+    <div className="w-full max-w-screen overflow-hidden">
+      <Swiper
+        modules={[Navigation, EffectCards]}
+        navigation
+        pagination={{ clickable: true }}
+        spaceBetween={50}
+        effect={'cards'}
+        grabCursor={true}
+        initialSlide={1}
+        className="mySwiper"
+        slidesPerView={1}
+        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}>
+        {swiperData.map((item, index) => (
+          <SwiperSlide key={item.id}>
+            <div className={activeIndex === index ? '' : 'mt-20'}>
+              <div className="flex flex-col items-center">
+                {activeIndex === index && (
+                  <div className="text-[#00C696] font-bold text-lg bg-[#E9F7EF] flex items-center m-auto px-4 rounded-lg py-2">
+                    <SvgDollarIcon />
+                    <span>{item.topSpan}</span>
+                  </div>
+                )}
+                {activeIndex === index && <SvgArrowDown />}
+                <div
+                  className={`px-3 py-9 md:p-9 w-[250px] md:w-[300px] flex flex-col items-center rounded-xl shadow-lg ${
+                    activeIndex === index ? 'bg-white' : 'bg-[#EDFCFF]' // Conditional background color
+                  }`}>
+                  <Image
+                    src={item.proImage}
+                    alt={`${item.name}'s profile image`}
+                    objectFit="contain"
+                    quality={100}
+                    width={120}
+                    height={120}
+                  />
+                  <h3 className="text-[#24252F] font-black text-2xl mt-4">{item.name}</h3>
+                  <p className="font-black text-[#4A77FF] text-base">{item.experience}</p>
+                  <div className="mt-9 flex gap-1 items-center justify-center flex-wrap">
+                    {item.skills.map((skill, index) => (
+                      <p
+                        key={index}
+                        className="border rounded-md border-[#C1C5CF] text-sm w-fit px-3 py-1 flex items-center font-black text-[#5E626F]">
+                        {skill}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      </div>
+
+      <div className="md:hidden mx-5 mt-8 gap-2 grid grid-cols-[110px_130px]">
+        <div className="flex md:hidden gap-2">
+        <SvgTickIcon />
+          <span>한국어 능력</span>
+        </div>
+        <div className="flex md:hidden  gap-2">
+          <SvgTickIcon />
+          <span>업무 수행 능력</span>
+        </div>
+        <div className="flex md:hidden  gap-2">
+          <SvgTickIcon />
+          <span>겸업 여부</span>
+        </div>
+        <div className="flex md:hidden  gap-2">
+           <SvgTickIcon />
+          <span>평판 조회</span>
+        </div>
+      </div>
+      <p className="md:hidden mx-5 pt-6 text-[#FBFF23] font-black underline underline-offset-2">
+        개발자가 필요하신가요?
+      </p>
+    </>
+  );
+};
+
+export default SwiperSection;
