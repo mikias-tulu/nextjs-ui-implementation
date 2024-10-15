1b6dacdc930b7cd98760f1319cf41d2eedcd3cd8
mikias-tulu <miki.tulu@gmail.com>
2024-10-15 13:42:35 +0300

    atomize all svg into files

---
+++ components/svgs/svgDashIcon.tsx
@@ -0,0 +1,22 @@
+const svgDashIcon: React.FC = () => {
+    return (
+      <>
+       <svg
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              stroke="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M5 7.5C5 7.22386 5.22386 7 5.5 7H9.5C9.77614 7 10 7.22386 10 7.5C10 7.77614 9.77614 8 9.5 8H5.5C5.22386 8 5 7.77614 5 7.5Z"
+                fill="#000000"
+              />
+        </svg> 
+      </>
+    );
+  };
+  export default svgDashIcon;
+  
